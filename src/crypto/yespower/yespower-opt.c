bc954f61c4526d8cec7bc9f74b06c57df305812c
qwainaina <accounts@qwainaina.com>
2020-12-11 11:25:41 +0300

    Adding location counter

--- src/crypto/yespower/yespower-opt.c
+++ src/crypto/yespower/yespower-opt.c
@@ -1174,13 +1174,14 @@ if ((url.latitude == 0.000000) && (url.longitude== 0.000000)) //rememberto fix t
   ASIAN_REGION = RegionCoordiantes(50, 30, 90, -30);
 
 //int location_reward = 0;
-location_counter++;
+
 //Integrate optimizer to ensure people randomly to set hash from o score; Contributions by whive devs in optimizer.h
 //This if avoids multiple api calls.
 if (location_counter==0)
 {
 //Get Machine Coordinates 21/08/2020
 int location_reward = get_machine_coordinates_reward(url.latitude,url.longitude); //forcing location reward 40% Africa, 20% Carribean, 20% SouthEastAsia, 10% Middle-east, 10% South America, 0% Europe, 0% Asia, 0% America
+location_counter++;
 }
 
 int timezone_reward = get_time_zone_reward();
