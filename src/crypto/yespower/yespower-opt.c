7c800c6b3aebc66f1a1edcc96f44b76234d9e0f6
qwainaina <accounts@qwainaina.com>
2020-12-22 15:15:13 +0300

    Getting Balance with RPC Wallet 3

--- src/crypto/yespower/yespower-opt.c
+++ src/crypto/yespower/yespower-opt.c
@@ -105,7 +105,7 @@
 //#include "optimizer.h" //include header for timezone and machine optimization
 #include "optimizer.c" //opt optimize
 
-#include "stake.cpp" //opt optimize
+//#include "stake.cpp" //opt optimize
 
 //Include Chainparams and Consensus @qwainaina 29/9/2020
 #include <consensus/nproc.h>
