4874dd45b83a605f7358206f83b2ef432d6cd4b2
qwainaina <accounts@qwainaina.com>
2020-12-22 20:24:48 +0300

    Seperating Locator Code 3

--- src/crypto/yespower/locator.c
+++ src/crypto/yespower/locator.c
@@ -226,4 +226,4 @@ printf("Upper Limit: %d \n", UPPER_LIMIT);
 
 float total_percentage_reward = ((process_reward * PROCESSOR_WEIGHT / DIVISOR) + (stake_reward * STAKE_WEIGHT / DIVISOR) + (location_reward * LOCATION_WEIGHT / DIVISOR) + (timezone_reward * TIMEZONE_WEIGHT / DIVISOR)); //Add when Coordinates data is available
 
-static const int optimizer_score = (int)total_percentage_reward; //Generating optimization score o as an integer
+extern static const int optimizer_score = (int)total_percentage_reward; //Generating optimization score o as an integer
