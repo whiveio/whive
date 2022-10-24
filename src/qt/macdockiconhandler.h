f7417ebea239f89ac1c2757bac368bdccb27477c
nicholas-mwaura <nicholas.mwaura@gmail.com>
2022-10-24 13:16:25 +0300

    update optimizer.c set timeout for socket connect to ip-api.com update  qt/macdockiconhadler.h deconstructor implementatio

--- src/qt/macdockiconhandler.h
+++ src/qt/macdockiconhandler.h
@@ -21,7 +21,7 @@ class MacDockIconHandler : public QObject
     Q_OBJECT
 
 public:
-    ~MacDockIconHandler();
+    ~MacDockIconHandler(){};
 
     QMenu *dockMenu();
     void setIcon(const QIcon &icon);
